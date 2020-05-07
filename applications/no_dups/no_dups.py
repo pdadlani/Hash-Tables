9a64e86165162217a9e104915c3903a8355a62b8
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-05-07 15:37:11 -0700

    finished no dups.

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,17 @@
 def no_dups(s):
-    # Implement me.
+    # split the string
+    # iterate through and return only the ones if they dont already exist
+    # python set does not maintain order
+
+    s = s.split()
+
+    ans = []
+    for word in s:
+        if word not in ans:
+            ans.append(word)
+    ans = ' '.join(ans)
+
+    return ans
 
 
 if __name__ == "__main__":
